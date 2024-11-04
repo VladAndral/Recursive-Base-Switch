617b00d8500cb716cc4cf78d83b3ae43f00e69c8
Vladimir Andral (Jr.) <andralvladimir@gmail.com>
2024-11-03 19:35:35 -0800

    Formatting

--- baseSwitch.cpp
+++ baseSwitch.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 string switchBase(int numBase10, int baseToSwitchTo);
 void recursive_switchBase(int numBase10, int baseToSwitchTo, string& currentString);
+string digitArray = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-=+"; // Easier way to convert up to base 50. We only need to create it once
 
 int main() {
 
     cout << "Enter base 10 number" << endl;
     int base10Number;
     cin >> base10Number;
-    cout << "Enter base to switch to [2-36]" << endl;
+    cout << "Enter base to switch to [2-" << digitArray.size() << "]" << endl;
     int baseToSwitchTo;
     cin >> baseToSwitchTo;
 
@@ -31,7 +32,6 @@ string switchBase(int numBase10, int baseToSwitchTo) {
 
 void recursive_switchBase(int numBase10, int baseToSwitchTo, string& currentString) {
 
-    string static digitArray = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Easier way to convert up to base 36. We only need to create it once
     if (numBase10 < baseToSwitchTo) {
         currentString += digitArray[numBase10];
     } else {
